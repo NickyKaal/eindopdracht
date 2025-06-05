71028a17e9ffe503d1a3ce3f589429635bf667a4
Nicky Kaal <nick.s.kaal@gmail.com>
2025-06-05 13:48:47 +0200

    Begin typografie

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import LoginPage from "./components/auth/LoginPage.jsx";
 
 function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
+      <LoginPage/>
     </>
   )
 }
